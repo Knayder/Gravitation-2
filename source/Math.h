1d51c8ce87a3304e695f324effefe5089876dfe0
Knayder <knayder@outlook.com>
2017-07-03 23:30:15 +0200

    Too lazy to write what Ive done

--- source/Math.h
+++ source/Math.h
@@ -3,7 +3,15 @@
 class Math final {
 public:
 	static float distance(const sf::Vector2f &a, const sf::Vector2f &b) {
-		sf::Vector2f d = a - b;
+		sf::Vector2f d = deltaVector(a, b);
 		return std::sqrt( d.x*d.x + d.y*d.y );
+	}
+
+	static sf::Vector2f deltaVector(const sf::Vector2f &a, const sf::Vector2f &b) {
+		return b - a;
+	}
+
+	static sf::Vector2f normalizedVector(const sf::Vector2f &a) {
+		return a / std::sqrt(a.x*a.x + a.y*a.y);
 	}
 };
